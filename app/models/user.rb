11496aded03604831b986ac8e8a33fa0c5efef6d
Sarah Shin <sarahsujin.shin@gmail.com>
2019-02-12 15:13:59 -0500

    update User model with heart_strings and user_chats connections

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,8 +1,9 @@
 class User < ApplicationRecord
-  has_many :heartstrings
-  has_many :tomodachis, through: :heartstrings
-  has_many :inverse_heartstrings, class_name: "User"
-  has_many :inverse_tomodachi, through: :inverse_heartstrings, source: "User"
-
+  has_many :heart_strings
+  has_many :tomodachis, through: :heart_strings
+  has_many :inverse_heart_strings, class_name: "User"
+  has_many :inverse_tomodachi, through: :inverse_heart_strings, source: "User"
+  has_many :user_chats
+  has_many :chats, through: :user_chats
 
 end
