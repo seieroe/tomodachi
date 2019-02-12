1a511255d92b1513d52c1237bb7c77c542796448
Sarah Shin <sarahsujin.shin@gmail.com>
2019-02-12 17:26:45 -0500

    create new users

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,2 +1,29 @@
 class UsersController < ApplicationController
+
+  def new
+    @user = User.new
+  end
+
+  def create
+    @user = User.new(user_params)
+    if @user.valid?
+      @user.save
+      redirect_to user_path(@user)
+    else
+      flash[:errors] = @user.errors.full_messages
+      redirect_to new_user_path
+    end
+  end
+
+  def show
+    @user = User.find(params[:id])
+  end
+
+  private
+
+  #strong params
+  def user_params
+    params.require(:user).permit(:name, :email, :password)
+  end
+
 end
