f66f2ff08764c3b426081e628d32a670040582c7
Sarah Shin <sarahsujin.shin@gmail.com>
2019-02-14 10:35:44 -0500

    add auth

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -10,14 +10,15 @@ class SessionsController < ApplicationController
       session[:user_id] = @user.id
       redirect_to @user
     else
-      flash[:notice] = 'Invalid username or password'
+      flash[:notice] = 'Invalid username or password. Please try again.'
       redirect_to login_path
     end
   end
 
   def destroy
     session[:user_id] = nil
-    flash[:notice] = 'u logged out'
+    flash[:notice] = "You've successfully logged out!Come back soon! 💕 "
     redirect_to login_path
   end
+
 end
