0dc3914f77a0d2e58f419b5d58400f29585a1250
github username <github email address>
2019-02-15 09:14:50 -0500

    fix user show page chat message and css

--- app/controllers/heart_strings_controller.rb
+++ app/controllers/heart_strings_controller.rb
@@ -16,9 +16,10 @@ class HeartStringsController < ApplicationController
   end
 
   def show
-    # byebug
+
     @heart_string = HeartString.find(params[:id])
     @photo = Photo.new(heart_string: @heart_string)
+
   end
 
   def chat
